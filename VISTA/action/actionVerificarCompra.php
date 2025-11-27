e514271e4f41c19afea7106b26195e47ad38ec6f
nasusxd <js8918657@gmail.com>
2025-11-27 15:36:27 -0300

    adawd

--- VISTA/action/actionVerificarCompra.php
+++ VISTA/action/actionVerificarCompra.php
@@ -1,21 +1,14 @@
 <?php
-header('Content-Type: application/json'); 
+header('Content-Type: application/json');
 include_once "../../configuracion.php";
 
-$mail = $_POST['mail'] ?? '';
+$mail = $_POST['mail'] ?? "";
 
-$session = new Session();
-$idUsuario = $session->getUsuario();
-$carrito = $session->getCarrito();
+$abmUsuario = new ABMUsuario();
+$usuario = $abmUsuario->buscar(["usmail" => $mail]);
 
-$abmCompra = new ABMCompra();
 
-$response = $abmCompra->procesarCompra($mail, $idUsuario, $carrito);
-
-// Si la compra fue exitosa → limpiar carrito
-if ($response['success']) {
-    $session->limpiarCarrito();
-}
-
-echo json_encode($response);
+echo json_encode([
+    "existe" => !empty($usuario)
+]);
 exit;
